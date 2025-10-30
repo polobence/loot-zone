ddc94ee327c308992cdd67a7e3779dd5e5075e3a
polobence <polovicsbence@gmail.com>
2025-10-30 14:23:23 +0100

    create product repository interface

---
+++ backend/src/main/java/com/lootzone/backend/repository/ProductRepository.java
@@ -0,0 +1,9 @@
+package com.lootzone.backend.repository;
+
+import com.lootzone.backend.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
