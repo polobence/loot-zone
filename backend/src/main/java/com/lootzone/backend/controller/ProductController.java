17a70f8a62d65bd98edee59b54b340ca0da0d183
polobence <polovicsbence@gmail.com>
2025-10-30 15:07:55 +0100

    delete crossorigin annotation from product controller

--- backend/src/main/java/com/lootzone/backend/controller/ProductController.java
+++ backend/src/main/java/com/lootzone/backend/controller/ProductController.java
@@ -9,7 +9,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "http://localhost:3000")
 public class ProductController {
 
     private final ProductService productService;
